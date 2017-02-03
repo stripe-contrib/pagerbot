bdd19e6469bb3a80c0dc1cb1df88f7112a5dd441
Karl-Aksel Puulmann <karl@heapanalytics.com>
2017-02-03 16:02:31 +0200

    Get rid of warnings caused by rtm adapter.

--- lib/pagerbot/slack_rtm_adapter.rb
+++ lib/pagerbot/slack_rtm_adapter.rb
@@ -21,12 +21,10 @@ module PagerBot
 
     def run!
       @client.on :message do |data|
-        if data.type == 'message'
-          if data.subtype == 'message_changed'
-            data.message.channel = data.channel
-            data = data.message
-          end
-          process_message(data)
+        if data.type == 'message' && data.subtype == 'message_changed'
+          process_message data.message, data.channel
+        elsif data.type == 'message' && data.subtype.nil?
+          process_message data, data.channel
         end
       end
       @client.start!
@@ -37,7 +35,7 @@ module PagerBot
       @user_cache[user_id]
     end
 
-    def process_message(message)
+    def process_message(message, channel)
       # Ignore messages from bots and self.
       return if message.subtype == 'bot_message'
       return if message.user == @client.self.id
@@ -49,7 +47,7 @@ module PagerBot
 
       usernames = [@client.self.name, "<@#{@client.self.id}>"]
       # In direct messages, don't require the bot name.
-      if message.channel.start_with? 'D'
+      if channel.start_with? 'D'
         usernames.push nil
       end
 
@@ -62,7 +60,7 @@ module PagerBot
         dm_channel = "@#{find_user(message.user).name}"
         send_message response.fetch(:private_message), dm_channel
       else
-        send_message response.fetch(:message), message.channel
+        send_message response.fetch(:message), channel
       end
     end
 
